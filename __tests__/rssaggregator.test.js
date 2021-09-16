00d60a9b4bdfa5822bee80788fb5edef426a15ac
Andrey Gunderin <ga@web-notes.info>
2021-09-17 02:03:58 +0300

    chore: removing unnecessary files

--- __tests__/rssaggregator.test.js
+++
@@ -1,7 +0,0 @@
-import testFunc from '../src/someFunc.js';
-
-test('Simple initial test', () => {
-  expect(testFunc(2)).toBe(4);
-  expect(testFunc(3)).toBe(9);
-  expect(testFunc(4)).toBe(16);
-});
