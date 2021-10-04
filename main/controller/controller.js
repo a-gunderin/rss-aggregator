cd1e6600e29a5ae605c8b19a910184e31fc65afa
Andrey Gunderin <ga@web-notes.info>
2021-10-04 21:36:09 +0300

    fix: entry point

--- main/controller/controller.js
+++ main/controller/controller.js
@@ -7,61 +7,63 @@ import updateRss from './updateRss.js';
 
 export default () => {
   const schema = yup.string().url();
-  elements.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputData = formData.get('url');
-    schema
-      .validate(inputData)
-      .then(() => {
-        watchedState.isValidUrl = true;
-      })
-      .then(() => {
-        if (watchedState.feedUrls.includes(inputData)) {
-          watchedState.isInFeed = true;
-          return false;
-        }
-        watchedState.isInFeed = false;
-        const proxyUrl = `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${inputData}`;
-        watchedState.rssIsLoading = true;
-        axios.get(proxyUrl)
-          .then((response) => {
-            watchedState.rssLoaded = false;
-            if (response.status === 200) {
-              const data = rssParser(response.data.contents);
-              if (data === 'parserError') {
-                watchedState.isValidRss = false;
+  document.addEventListener('DOMContentLoaded', () => {
+    elements.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const inputData = formData.get('url');
+      schema
+        .validate(inputData)
+        .then(() => {
+          watchedState.isValidUrl = true;
+        })
+        .then(() => {
+          if (watchedState.feedUrls.includes(inputData)) {
+            watchedState.isInFeed = true;
+            return false;
+          }
+          watchedState.isInFeed = false;
+          const proxyUrl = `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${inputData}`;
+          watchedState.rssIsLoading = true;
+          axios.get(proxyUrl)
+            .then((response) => {
+              watchedState.rssLoaded = false;
+              if (response.status === 200) {
+                const data = rssParser(response.data.contents);
+                if (data === 'parserError') {
+                  watchedState.isValidRss = false;
+                  return false;
+                }
+                watchedState.rssLoaded = true;
+                watchedState.feedUrls.push(inputData);
+                watchedState.feeds.push({ title: data.title, description: data.description });
+                watchedState.posts.push(...data.items);
                 return false;
               }
-              watchedState.rssLoaded = true;
-              watchedState.feedUrls.push(inputData);
-              watchedState.feeds.push({ title: data.title, description: data.description });
-              watchedState.posts.push(...data.items);
               return false;
-            }
-            return false;
-          })
-          .then(() => {
-            watchedState.rssIsLoading = false;
-            updateRss();
-          });
-        return false;
-      })
-      .catch(() => {
-        watchedState.isValidUrl = false;
-        watchedState.rssIsLoading = false;
-      });
-  });
+            })
+            .then(() => {
+              watchedState.rssIsLoading = false;
+              updateRss();
+            });
+          return false;
+        })
+        .catch(() => {
+          watchedState.isValidUrl = false;
+          watchedState.rssIsLoading = false;
+        });
+    });
 
-  elements.postsList.addEventListener('click', (e) => {
-    const { target } = e;
-    if (target.tagName === 'A') {
-      watchedState.viewedArticles.push(target.href);
-    }
-    if (target.tagName === 'BUTTON') {
-      watchedState.viewedArticles.push(target.previousElementSibling.href);
-      watchedState.activePostInModal = target.previousElementSibling.href;
-    }
-    return false;
+    elements.postsList.addEventListener('click', (e) => {
+      const { target } = e;
+      if (target.tagName === 'A') {
+        watchedState.viewedArticles.push(target.href);
+      }
+      if (target.tagName === 'BUTTON') {
+        watchedState.viewedArticles.push(target.previousElementSibling.href);
+        watchedState.activePostInModal = target.previousElementSibling.href;
+      }
+      return false;
+    });
   });
 };
