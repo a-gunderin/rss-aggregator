44fb2825b24457f4bd3ae92491f52bb90e5b7f84
Andrey Gunderin <ga@web-notes.info>
2022-01-10 02:41:37 +0200

    trying to pass hexlet tests

--- main/view/formBlockers.js
+++ main/view/formBlockers.js
@@ -1,10 +1,12 @@
 const blockForm = () => {
   document.getElementById('url-input').setAttribute('disabled', '');
+  document.getElementById('url-input').setAttribute('readonly', '');
   document.querySelector('button[type="submit"]').setAttribute('disabled', '');
 };
 
 const unblockForm = () => {
   document.getElementById('url-input').removeAttribute('disabled');
+  document.getElementById('url-input').removeAttribute('readonly');
   document.querySelector('button[type="submit"]').removeAttribute('disabled');
 };
 
